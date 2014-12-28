f12aaf5b1b332cce22be3f311f78ae31867a1a83
Tim Paine <t.paine154@gmail.com>
2014-12-28 01:25:51 -0500

    Working with verilator now.

--- fair_queue/fq.sv
+++ fair_queue/fq.sv
@@ -19,14 +19,15 @@ module fq #(parameter NUM_IN_LOG2=3)
 	logic valid[2**NUM_IN_LOG2-1:0]; //input is ready
 	logic [NUM_IN_LOG2-1:0] pick; //which channel to select
 	logic valid_o; //selected channel is valid 
+	logic [7:0] size;
 
-	logic [7:0] counts [2**NUM_IN_LOG2-1:0]; //first 8 bits of each channel (count)
-	logic [2**NUM_IN_LOG2-1:0] rdreq;
+	integer t; //virtual time
+
+	logic [31:0] counts [2**NUM_IN_LOG2-1:0]; //first 8 bits of each channel (count)
 
 genvar j;
 generate for(j=0;j<2**NUM_IN_LOG2;j=j+1) begin
-	assign counts[j] = fifo_data[j][7:0];
-	assign rdreq[j] = fifo_rdreq[j];
+	assign counts[j] = t+{24'b0,fifo_data[j][7:0]};
 end
 endgenerate
 
@@ -39,7 +40,7 @@ endgenerate
 
 	//queue_up selects the next packet to send
 	queue_up q_inst(.valid_i(valid), .count(counts),
-		.pick(pick), .valid_o(valid_o),.*);
+		.pick(pick), .valid_o(valid_o),.size(size),.*);
 
 
 
@@ -49,9 +50,11 @@ generate for(i=0;i<2**NUM_IN_LOG2;i=i+1) begin
 	always_ff @(posedge clk) begin
 		if(rst) begin
 			valid[i] <= 1'b0;
+			//counts[i]<='b0;
 		end else begin
 			if(!fifo_empty[i] && !running[i]) begin //available for selection
 				valid[i] <= 1'b1;
+				//counts[i] <= t+{24'b0,fifo_data[i][7:0]};
 			end else begin 
 				valid[i] <= 1'b0; //not available for selection
 				if(fifo_empty[i]) begin
@@ -74,18 +77,33 @@ endgenerate
 			running <= 'b0;
 			output_data <= 'b0;
 			output_data_valid <= 'b0;
-			rdreq <= 'b0;
+			/* this is a stupid syntax thing */
+			fifo_rdreq[0] <= 'b0;
+			fifo_rdreq[1] <= 'b0;
+			fifo_rdreq[2] <= 'b0;
+			fifo_rdreq[3] <= 'b0;
+			fifo_rdreq[4] <= 'b0;
+			fifo_rdreq[5] <= 'b0;
+			fifo_rdreq[6] <= 'b0;
+			fifo_rdreq[7] <= 'b0;
+
+			total <= 'b0;
+			count <= 'b0;
+			t <= 0;
 		end else begin
 			//change this if additional output channels
 			if(&running) begin //something is already outputting
 				//output record
 				output_data <= fifo_data[current];
 				output_data_valid <= 1'b1;
-				rdreq[current] <= 1'b1;
+				fifo_rdreq[current] <= 1'b1;
+				//set others to 0
+				count <= count + 1;
+				t <= t+1;
 
-				//check if last packet
+				//check if last packet to avoid 1 cycle bubble
 				if((count+1)==total) begin //last packet 
-					running[current] <= 1'b0;
+					running[current] <= 1'b0;					
 				end
 			end else begin
 				if(valid_o) begin //something selected to be output
@@ -93,11 +111,24 @@ endgenerate
 					running[pick] <= 1'b1; //mux on output from submodule
 					output_data <= fifo_data[pick];
 					output_data_valid <= 1'b1;
-					rdreq[pick] <= 1'b1;
+					fifo_rdreq[pick] <= 1'b1;
+					//set others to 0
+					total <= size;
+					count <= 8'b01;
 				end else begin
-					rdreq <= 'b0;
+					/* this is a stupid syntax thing */
+					fifo_rdreq[0] <= 'b0;
+					fifo_rdreq[1] <= 'b0;
+					fifo_rdreq[2] <= 'b0;
+					fifo_rdreq[3] <= 'b0;
+					fifo_rdreq[4] <= 'b0;
+					fifo_rdreq[5] <= 'b0;
+					fifo_rdreq[6] <= 'b0;
+					fifo_rdreq[7] <= 'b0;
 					current <= 'b0;
 					output_data_valid <= 1'b0;
+					total <= 'b0;
+					count <= 'b0;
 				end
 			end
 		end
--- fair_queue/queue_up.sv
+++ fair_queue/queue_up.sv
@@ -5,13 +5,34 @@ module queue_up #(parameter NUM_IN_LOG2=3)
 (
 	clk, rst,
 	count, valid_i,
-	pick, valid_o
+	pick, valid_o, size
 );
 	input logic clk, rst;
-	input logic [7:0] count [2**NUM_IN_LOG2-1:0];
+	input logic [31:0] count [2**NUM_IN_LOG2-1:0];
 	input logic valid_i [2**NUM_IN_LOG2-1:0];
 	output logic [NUM_IN_LOG2-1:0] pick;
+	output logic [7:0] size;
 	output logic valid_o; 
 
+	integer min_t; //used to determine when 
+				   //integer wraparound is 
+				   //occuring
+
+	always_ff @(posedge clk) begin
+		if(rst) begin
+			pick <= 'b0;
+			size <= 'b0;
+			valid_o <= 'b0;
+			min_t <= 0;
+		end else begin
+			if(|valid_i) begin
+				//nice disgusting combinational block
+				if(&count) begin
+				end
+				if(&min_t) begin
+				end
+			end
+		end
+	end
 
 endmodule
