d30c3158a27c897e5a2594c2da8a1a20b46eb99e
Tim Paine <t.paine154@gmail.com>
2014-12-28 20:36:19 -0500

    Comments added, bugs identified.

--- fair_queue/fq.sv
+++ fair_queue/fq.sv
@@ -46,20 +46,25 @@ endgenerate
 
 
 
+/* 
+ * Send each new packet to the 
+ * queue up module along with 
+ * its virtual end time
+ */
 genvar i;
 generate for(i=0;i<2**NUM_IN_LOG2;i=i+1) begin
 	always_ff @(posedge clk) begin
 		if(rst) begin
-			valid[i] <= 1'b0;
+			valid[i] <= 1'b0; 
 			counts[i]<='b0;
 		end else begin
 			if(!fifo_empty[i] && !running[i]) begin //available for selection
-				valid[i] <= 1'b1;
+				valid[i] <= 1'b1; //valid if it is available for selection
 				if(!valid[i]) begin //need to enqueue
-					counts[i] <= t+{24'b0,fifo_data[i][7:0]};
+					counts[i] <= t+{24'b0,fifo_data[i][7:0]}; //add virtual time
 				end
 			end else begin 
-				valid[i] <= 1'b0; //not available for selection
+				valid[i] <= 1'b0; //not available for selection (already picked)
 				if(fifo_empty[i]) begin
 				//do nothing
 				end else if(running[i]) begin
@@ -76,10 +81,11 @@ endgenerate
 
 	always_ff @(posedge clk) begin
 		if(rst) begin
-			current <= 'b0;
-			running <= 'b0;
+			current <= 'b0; //currently sending
+			running <= 'b0; //currently sending 
 			output_data <= 'b0;
 			output_data_valid <= 'b0;
+
 			/* this is a stupid syntax thing */
 			fifo_rdreq[0] <= 'b0;
 			fifo_rdreq[1] <= 'b0;
@@ -90,38 +96,101 @@ endgenerate
 			fifo_rdreq[6] <= 'b0;
 			fifo_rdreq[7] <= 'b0;
 
-			total <= 'b0;
-			count <= 'b0;
-			t <= 0;
+			total <= 'b0; //total packets in current submission
+			count <= 'b0; //current packet number
+			t <= 0; //virtual time
 		end else begin
 			//change this if additional output channels
+			//if(|running) doesn't work for unpacked
 			if(running[0] || running[1] || running[2] ||
 				running[3] || running[4] || running[5]||
 				running[6] || running[7]) begin //something is already outputting
+				
 				//output record
 				output_data <= fifo_data[current];
 				output_data_valid <= 1'b1;
 				fifo_rdreq[current] <= 1'b1;
+
 				//set others to 0
+				if(pick!=0) begin
+					fifo_rdreq[0] <= 1'b0;
+				end
+				if(pick!=1) begin
+					fifo_rdreq[1] <= 1'b0;
+				end
+				if(pick!=2) begin
+					fifo_rdreq[2] <= 1'b0;
+				end
+				if(pick!=3) begin
+					fifo_rdreq[3] <= 1'b0;
+				end
+				if(pick!=4) begin
+					fifo_rdreq[4] <= 1'b0;
+				end
+				if(pick!=5) begin
+					fifo_rdreq[5] <= 1'b0;
+				end
+				if(pick!=6) begin
+					fifo_rdreq[6] <= 1'b0;
+				end
+				if(pick!=7) begin
+					fifo_rdreq[7] <= 1'b0;
+				end
+
+				//increment packet count
 				count <= count + 1'b1;
+				//increment virtual time
 				t <= t+1;
 
 				//check if last packet to avoid 1 cycle bubble
-				if((count+1'b1)==total) begin //last packet 
-					running[current] <= 1'b0;
-					count <= 'b0;					
+				if((count+1'b1)>=total) begin //last packet 
+					running[current] <= 1'b0; //turn off current
+					count <= 'b0; //reset count
+
+					//fetch next
+					//TODO
+
 				end
 			end else begin
 				if(valid_o) begin //something selected to be output
-					current <= pick;
+					current <= pick; //output from queue_up
 					running[pick] <= 1'b1; //mux on output from submodule
-					output_data <= fifo_data[pick];
-					output_data_valid <= 1'b1;
-					fifo_rdreq[pick] <= 1'b1;
+					output_data <= fifo_data[pick]; //output first packet
+					output_data_valid <= 1'b1; //output is valid
+					fifo_rdreq[pick] <= 1'b1; //raise ACK
+
 					//set others to 0
-					total <= fifo_data[pick][7:0];
-					count <= 8'b01;
-				end else begin
+					if(pick!=0) begin
+						fifo_rdreq[0] <= 1'b0;
+					end
+					if(pick!=1) begin
+						fifo_rdreq[1] <= 1'b0;
+					end
+					if(pick!=2) begin
+						fifo_rdreq[2] <= 1'b0;
+					end
+					if(pick!=3) begin
+						fifo_rdreq[3] <= 1'b0;
+					end
+					if(pick!=4) begin
+						fifo_rdreq[4] <= 1'b0;
+					end
+					if(pick!=5) begin
+						fifo_rdreq[5] <= 1'b0;
+					end
+					if(pick!=6) begin
+						fifo_rdreq[6] <= 1'b0;
+					end
+					if(pick!=7) begin
+						fifo_rdreq[7] <= 1'b0;
+					end
+
+					//TODO check for corner case of 
+					//only single packet transmission
+					total <= fifo_data[pick][7:0]; //get total
+					count <= 8'b01;//set count to 1
+
+				end else begin //essentially a reset of state
 					/* this is a stupid syntax thing */
 					fifo_rdreq[0] <= 'b0;
 					fifo_rdreq[1] <= 'b0;
@@ -131,10 +200,14 @@ endgenerate
 					fifo_rdreq[5] <= 'b0;
 					fifo_rdreq[6] <= 'b0;
 					fifo_rdreq[7] <= 'b0;
+					
+					//not currently outputting
 					current <= 'b0;
 					output_data_valid <= 1'b0;
-					total <= 'b0;
-					count <= 'b0;
+
+
+					total <= 'b0; //reset total
+					count <= 'b0; //reset count
 				end
 			end
 		end
--- fair_queue/queue_up.sv
+++ fair_queue/queue_up.sv
@@ -8,15 +8,14 @@ module queue_up #(parameter NUM_IN_LOG2=3)
 	pick, valid_o
 );
 	input logic clk, rst;
-	input logic [31:0] count [2**NUM_IN_LOG2-1:0];
-	input logic valid_i [2**NUM_IN_LOG2-1:0];
-	output logic [NUM_IN_LOG2-1:0] pick;
-	//output logic [7:0] size;
-	output logic valid_o; 
+	input logic [31:0] count [2**NUM_IN_LOG2-1:0]; //sizes
+	input logic valid_i [2**NUM_IN_LOG2-1:0]; //input is valid
+	output logic [NUM_IN_LOG2-1:0] pick; //smallest record
+	output logic valid_o;  //output is valid
 
 	integer unsigned min_t; //used to determine when 
-				   //integer wraparound is 
-				   //occuring
+						   //integer wraparound is 
+						   //occuring
 
     logic [31:0] min_size;
     logic [NUM_IN_LOG2-1:0] min_index;
@@ -25,6 +24,7 @@ module queue_up #(parameter NUM_IN_LOG2=3)
     always_comb begin
     	min_size = 32'b1111_1111_1111_1111_1111_1111_1111_1111; //not correct
 
+    	//big block, there is likely a race condition here
     	if(valid_i[0] && count[0]<min_size) begin
     		min_size=count[0];
     		min_index = 3'b000;
@@ -63,8 +63,7 @@ module queue_up #(parameter NUM_IN_LOG2=3)
 
 	always_ff @(posedge clk) begin
 		if(rst) begin
-			pick <= 'b0;
-			//size <= 'b0;
+			pick <= 'b0; 
 			valid_o <= 1'b0;
 			min_t <= 0;
 		end else begin
@@ -72,9 +71,9 @@ module queue_up #(parameter NUM_IN_LOG2=3)
 			if(valid_i[0] || valid_i[1] || valid_i[2] ||
 				valid_i[3] || valid_i[4] || valid_i[5] ||
 				valid_i[6] || valid_i[7]) begin //if any valid inputs
-				//size <= min_size;
 				pick <= min_index;
 				valid_o <= 1'b1;
+				//TODO handle wraparound
 				if(&min_t) begin
 				end
 			end else begin
